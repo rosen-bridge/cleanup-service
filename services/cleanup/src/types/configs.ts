5ea7723f11df2ee7a590b56ba0320d3dafd99d5f
Mehran Aghabozorgi <mehranagh200@gmail.com>
2026-01-30 15:09:33 -0800

    Use Config package

--- services/cleanup/src/types/configs.ts
+++ services/cleanup/src/types/configs.ts
@@ -1,71 +1,60 @@
-/**
- * Workflow configuration.
- */
-export interface WorkflowConfig {
-    readonly contractsPath: string;
-    readonly ergoNetwork: 'mainnet' | 'testnet';
-    readonly cleanupMnemonic: string;
-    readonly minBoxValue: bigint;
-    readonly minCleanupValue: bigint;
-    readonly txFee: string;
-  }
-  
-  
-  /**
-   * TxPot configuration.
-   */
-  export interface TxPotConfig {
-    readonly updateInterval: number; // seconds
-    readonly txRequiredConfirmations: number;
-  }
-  
-  
-  export interface ScannerConfig {
-    readonly updateInterval: number; // seconds
-    readonly nodeUrl: string;
-    readonly explorerUrl: string;
-    readonly initialHeight: number;
-  }
-  
-  /**
-   * Log types.
-   */
-  export type LogType = 'file' | 'console' | 'loki';
+export interface CleanupServiceConfig {
+  boxLookup: BoxLookup;
+  scanner: Scanner;
+  txpot: Txpot;
+  database: Database;
+  logs: Logs[];
+  workflow: Workflow;
+  intervals: Intervals;
+}
 
-  /**
-   * Log configuration.
-   */
-  export interface LogConfig {
-    readonly type: LogType;
-    readonly level: string;
-    readonly maxSize?: string;
-    readonly maxFiles?: string;
-    readonly path?: string;
-    readonly serviceName?: string;
-    readonly host?: string;
-    readonly basicAuth?: string;
-  }
-  
-  
-  /**
-   * Database configuration.
-   */
-  export interface DatabaseConfig {
-    readonly type: 'sqlite' | 'postgres';
-    readonly path?: string; // sqlite
-    readonly host?: string; // postgres
-    readonly port?: number; // postgres
-    readonly user?: string; // postgres
-    readonly password?: string; // postgres
-    readonly name?: string; // postgres
-  }
-  
-  
-  /**
-   * BoxLookup configuration.
-   */
-  export interface BoxLookupConfig {
-    readonly updateInterval: number; // seconds
-    readonly nodeUrl: string;
-  }
-  
+export interface Intervals {
+  workflow: number;
+}
+
+export interface Workflow {
+  contractsPath: string;
+  ergoNetwork: 'mainnet' | 'testnet';
+  cleanupMnemonic: string;
+  minBoxValue: bigint;
+  minCleanupValue: bigint;
+  txFee: string;
+}
+
+export interface Logs {
+  type: 'file' | 'console' | 'loki';
+  level: string;
+  path?: string;
+  maxSize?: string;
+  maxFiles?: string;
+  serviceName?: string;
+  host?: string;
+  basicAuth?: string;
+}
+
+export interface Database {
+  type: 'sqlite' | 'postgres';
+  path?: string;
+  host?: string;
+  port?: number;
+  user?: string;
+  password?: string;
+  name?: string;
+}
+
+export interface Txpot {
+  updateInterval: number;
+  txRequiredConfirmations: number;
+}
+
+export interface Scanner {
+  updateInterval: number;
+  nodeUrl: string;
+  explorerUrl: string;
+  initialHeight: number;
+}
+
+export interface BoxLookup {
+  updateInterval: number;
+  nodeUrl: string;
+}
